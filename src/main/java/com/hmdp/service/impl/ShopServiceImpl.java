e7f2f18e476071dac8685276d8cc1695317a9d32
HaishuoZhang <1415522673@qq.com>
2024-02-03 12:14:26 +0800

    解决缓存穿透，修改readme.md

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -10,13 +10,13 @@ import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -40,9 +40,18 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = BeanUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+
+        // 判断命中的是否为空值
+        if(shopJson != null) {
+            return Result.fail("信息不存在");
+        }
+
         // 从数据库中查数据
         Shop shop = getById(id);
         if(shop == null) {
+            // 保存空值，使用 缓存空对象 的方法解决缓存穿透
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+
             return Result.fail("店铺不存在");
         }
         // 保存到redis, 并设置过期时间为30min
@@ -51,6 +60,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     }
 
     @Override
+    @Transactional
     public Result update(Shop shop) {
         Long id = shop.getId();
         if(id == null) {
