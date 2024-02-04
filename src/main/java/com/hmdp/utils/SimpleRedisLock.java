35ba0ebc1f4412e79db586117a490d4c9780ac66
HaishuoZhang <1415522673@qq.com>
2024-02-04 16:06:33 +0800

    使用Lua脚本解决原子性问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -17,6 +20,13 @@ public class SimpleRedisLock implements ILock{
 
     private final static String KEY_PREFIX = "lock:";
     private final static String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private final static DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     @Override
     public boolean tryLock(long timeOutSec) {
@@ -31,6 +41,15 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
+        // 调用lua脚本
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
+    }
+
+    /*
+    @Override
+    public void unlock() {
         // 每次释放锁的时候判断当前调用者要删的锁是否是他的锁，防止把别人的锁删除掉。
         String value = ID_PREFIX + Thread.currentThread().getId();
         String s = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
@@ -38,4 +57,5 @@ public class SimpleRedisLock implements ILock{
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
     }
+    */
 }
