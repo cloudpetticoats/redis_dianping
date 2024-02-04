2167dc15060916b55a126c57b785270ef8c25811
HaishuoZhang <1415522673@qq.com>
2024-02-04 15:12:24 +0800

    解决不同线程锁误删问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -15,20 +16,26 @@ public class SimpleRedisLock implements ILock{
     }
 
     private final static String KEY_PREFIX = "lock:";
+    private final static String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     @Override
     public boolean tryLock(long timeOutSec) {
         // 获取当前调用线程标识
-        long value = Thread.currentThread().getId();
+        String value = ID_PREFIX + Thread.currentThread().getId();
         // 加锁
         Boolean isSuccess = stringRedisTemplate.opsForValue().
-                setIfAbsent(KEY_PREFIX + name, value + "", timeOutSec, TimeUnit.SECONDS);
+                setIfAbsent(KEY_PREFIX + name, value, timeOutSec, TimeUnit.SECONDS);
 
         return Boolean.TRUE.equals(isSuccess);
     }
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // 每次释放锁的时候判断当前调用者要删的锁是否是他的锁，防止把别人的锁删除掉。
+        String value = ID_PREFIX + Thread.currentThread().getId();
+        String s = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        if(value.equals(s)) {
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
