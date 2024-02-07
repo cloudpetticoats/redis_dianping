87aa524a9898817c1d5a36fcf057a7b67e8b9b90
HaishuoZhang <1415522673@qq.com>
2024-02-07 10:01:31 +0800

    完成关注和取关模块

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,20 +1,35 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
  *  前端控制器
  * </p>
  *
- * @author 虎哥
- * @since 2021-12-22
+ * @author Zhang Haishuo
+ * @since 2024-2-7
  */
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+
+    @Resource
+    private IFollowService followService;
+
+    @PutMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable Long id, @PathVariable Boolean isFollow) {
+        return followService.follow(id, isFollow);
+    }
+
+    @GetMapping("/or/not/{id}")
+    public Result follow(@PathVariable Long id) {
+        return followService.isFollow(id);
+    }
 
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,20 +1,46 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
  *  服务实现类
  * </p>
  *
- * @author 虎哥
- * @since 2021-12-22
+ * @author Zhang Haishuo
+ * @since 2024-2-7
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Override
+    public Result follow(Long id, Boolean isFollow) {
+        Long userId = UserHolder.getUser().getId();
+        if(isFollow) {
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(id);
+            save(follow);
+        } else {
+            remove(new QueryWrapper<Follow>().
+                    eq("user_id", userId).eq("follow_user_id", id));
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result isFollow(Long id) {
+        Long userId = UserHolder.getUser().getId();
+        Integer count = query().eq("user_id", userId).eq("follow_user_id", id).count();
+        return Result.ok(count > 0);
+    }
 }
